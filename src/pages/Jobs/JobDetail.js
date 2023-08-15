c622f4ae52f06cb5104d55f1ac4734ee6120b1da
peymn88gh <peymn88gh@gmail.com>
2023-08-15 15:51:31 +0330

    task_158: second try for merge conflicts

--- src/pages/Jobs/JobDetail.js
+++ src/pages/Jobs/JobDetail.js
@@ -1,18 +1,39 @@
 import React from "react";
+import data from 'data/jobs.json';
 import { useParams } from "react-router-dom";
+import Contact from "pages/Contact";
 
-function JobDetail() {
-  const { jobId } = useParams();
-
-  // Fetch job details using the jobId, e.g., from an API
+const JobDetail = () => {
+  const {jobId} = useParams()
+  const job = data.jobs.find((obj)=>obj.id===jobId)
 
   return (
-    <div>
-      <h2>Job Details</h2>
-      <p>Details for Job</p>
-      {/* Display job details */}
-    </div>
+    <article className="py-12 px-8">
+      <h2 className="text-3xl font-bold mb-4">{job.role}</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div>
+          <img
+            src={job.img}
+            alt={job.role}
+            className="h-60 w-full object-cover rounded-lg shadow-md"
+          />
+        </div>
+        <div>
+          <h3 className="text-xl font-semibold mb-2">Job Description</h3>
+          <p className="text-gray-600 mb-6">{job.description}</p>
+
+          <h3 className="text-xl font-semibold mb-2">Requirements</h3>
+          <p className="text-gray-600 mb-6">
+            <strong>Must Have:</strong> {job.mustHave}
+          </p>
+          <p className="text-gray-600">
+            <strong>Nice to Have:</strong> {job.niceToHave}
+          </p>
+        </div>
+      </div>
+      <Contact applyInfo={job}/>
+    </article>
   );
-}
+};
 
 export default JobDetail;
--- src/pages/Jobs/Jobs.js
+++ src/pages/Jobs/Jobs.js
@@ -1,5 +1,34 @@
-export default function Jobs(){
-    return <>
-    <p>this is jobs page</p>
-    </>
-}
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Jobs = ({ data }) => {
+  return (
+    <article className="text-center py-12 mx-8">
+      <h2 className="text-3xl font-bold mb-8">Explore Job Opportunities</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {data.map((job) => (
+            <Link
+            key={job.id}
+            to={`/jobs/${job.id}`}
+            className="hover:shadow-lg transition duration-300 ease-in-out"
+            >
+            <section className="bg-white p-6 rounded-lg shadow-md">
+                <img
+                src={job.img}
+                alt={job.role}
+                className="h-24 w-24 mx-auto mb-4 rounded-full"
+                />
+                <h3 className="text-xl font-semibold mb-2">{job.role}</h3>
+                <p className="text-gray-600">{job.agileTeamRelated}</p>
+                <button className="mt-4 bg-red-300 hover:bg-red-400 text-white py-2 px-4 rounded-full focus:outline-none focus:ring focus:ring-blue-300">
+                Apply
+                </button>
+            </section>
+            </Link>
+        ))}
+        </div>
+    </article>
+  );
+};
+
+export default Jobs;
