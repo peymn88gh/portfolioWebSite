ea07d34d53a2ec58026230448d19db993ec34f2b
Saeed Mogharian <moghariansaeed@gmail.com>
2023-08-15 08:48:01 +0330

    revert home.js changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,11 @@
 import { Element } from 'react-scroll'
 import './Home.css'
-
 export default function(){
     return <>
         <div className="homebackground">
             <div className="content-container">
                 <h1 className=' font-bold text-5xl mb-10'>LeoNet Informatik GmbH</h1>
-                <button className='rounded-full text-lg bg-red-500 py-2 px-4 m-5'>
+                <button className='rounded-full text-lg bg-red-500 py-2 px-4 m-5 '>
                     LEARN MORE
                 </button>
                 <button className='rounded-full text-lg bg-red-500 py-2 px-4 '>
@@ -15,11 +14,11 @@ export default function(){
             </div>
         </div>
         <Element name="services" className="services bg-gray-100 py-10">
-           <div className='h-10'>
+            <div className='h-10'>
                 <p>this is services</p>
-            </div> 
+            </div>
         </Element>
-        
-        
+
+
     </>
 }
