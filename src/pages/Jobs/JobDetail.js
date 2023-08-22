e51ed3e7b46873c5e9f2983a4a5e27fddf363cef
matine <mghazanfari@tarasystem.ch>
2023-08-22 15:31:47 +0330

    [fix]-167-translate description and cleanup

--- src/pages/Jobs/JobDetail.js
+++ src/pages/Jobs/JobDetail.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
-import data from 'data/jobs.json';
 import { Link, useParams } from "react-router-dom";
 import Contact from "pages/Contact";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import {useTranslation} from "react-i18next";
 
 const JobDetail = () => {
   const {jobId} = useParams()
-  const job = data.jobs.find((obj)=>obj.id===jobId)
+
+
+    const [t] = useTranslation('jobs');
 // console.log(window.history);
+
   
   return (
     <article className="py-12 px-8">
@@ -22,29 +25,29 @@ const JobDetail = () => {
           Back
         </Link>
       </div>
-      <h2 className="text-3xl font-bold mb-4">{job.role}</h2>
+      <h2 className="text-3xl font-bold mb-4">{t(`${jobId}.role`)}</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div>
           <img
-            src={job.img}
-            alt={job.role}
+            src={t(`${jobId}.img`)}
+            alt={t(`${jobId}.role`)}
             className="h-60 w-full object-cover rounded-lg shadow-md"
           />
         </div>
         <div>
           <h3 className="text-xl font-semibold mb-2">Job Description</h3>
-          <p className="text-gray-600 mb-6">{job.description}</p>
+          <p className="text-gray-600 mb-6">{t(`${jobId}.description`)}</p>
 
           <h3 className="text-xl font-semibold mb-2">Requirements</h3>
           <p className="text-gray-600 mb-6">
-            <strong>Must Have:</strong> {job.mustHave}
+            <strong>Must Have:</strong> {t(`${jobId}.mustHave`)}
           </p>
           <p className="text-gray-600">
-            <strong>Nice to Have:</strong> {job.niceToHave}
+            <strong>Nice to Have:</strong> {t(`${jobId}.niceToHave`)}
           </p>
         </div>
       </div>
-      <Contact applyInfo={job}/>
+      <Contact applyInfo={t(jobId)}/>
     </article>
   );
 };
