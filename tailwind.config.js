d7aa0ede42b0b51ba3a4bdec56ff40dd607dbe9b
peymn88gh <peymn88gh@gmail.com>
2023-11-23 13:17:21 +0330

    menu-cleanUp-clip-animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '3.953rem',
+      '4xl': '4.441rem',
+      '5xl': '7.441rem',
+    },
     extend: {
+      colors: {
+        primary: '#BE123C', // Change this color to your primary color
+        secondary: '#172554', // Change this color to your secondary color
+        accent: '#FFFFFF', // Change this color to your accent color
+      },
       fontFamily: {
         sans: ['Lato', ...defaultTheme.fontFamily.sans],
+        bebas: ['"Bebas Neue"'],
       },
       boxShadow:{
         myShadow1: "4.1px -5px 0 0  rgb(241 245 249)",
